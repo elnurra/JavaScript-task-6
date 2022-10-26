e9d547669e5946498928a5e5e3b1bb5927cb75ac
elnurra <elnurra@code.edu.az>
2022-10-26 15:11:01 +0400

    Task not completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,22 +5,22 @@ const thirdEmail = "ali@outlook.com";
 const forthEmail = "nicat@email.ru";
 const fifthEmail = "aydan@asoiu.edu.az";
 emails.push(firstEmail, secondEmail, thirdEmail, forthEmail, fifthEmail);
-localStorage.setItem(firstEmail, "123");
-localStorage.setItem(secondEmail, "1234565yamate");
-localStorage.setItem(thirdEmail, "ali1234");
-localStorage.setItem(forthEmail, "niko1985");
-localStorage.setItem(fifthEmail, "aydan2004");
+// localStorage.setItem(firstEmail, "123");
+// localStorage.setItem(secondEmail, "1234565yamate");
+// localStorage.setItem(thirdEmail, "ali1234");
+// localStorage.setItem(forthEmail, "niko1985");
+// localStorage.setItem(fifthEmail, "aydan2004");
 
 const form = document.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
 const passwords = [];
-const firstPassword = localStorage.getItem(firstEmail);
-const secondPassword = localStorage.getItem(secondEmail);
-const thirdPassword = localStorage.getItem(thirdEmail);
-const fourthPassword = localStorage.getItem(forthEmail);
-const fifthPassword = localStorage.getItem(fifthEmail);
+const firstPassword = "123";
+const secondPassword = "1234";
+const thirdPassword = "12345";
+const fourthPassword = "123456";
+const fifthPassword = "1234567";
 passwords.push(
   firstPassword,
   secondPassword,
@@ -67,20 +67,58 @@ btn.addEventListener("click", () => {
       pPassword.style.display = "block";
       pPassword.innerText = `${password}`;
       btn.style.display = "none";
-      localStorage.setItem(emailInput, passwordInput);
+      localStorage.setItem(emailInput.value, passwordInput.value);
+      console.log(localStorage.getItem(emailInput.value));
     }, 1000);
   } else {
     alert("Parol yada login sehvdi");
   }
 });
+
 const interval = setInterval(() => {
-  if (localStorage.getItem(emailInput)) {
+  if (localStorage.getItem("elnur@code.edu.az")) {
     emailInput.style.display = "none";
     passwordInput.style.display = "none";
     pEmail.style.display = "block";
-    pEmail.innerText = `${email}`;
+    pEmail.innerText = `elnur@code.edu.az`;
     pPassword.style.display = "block";
-    pPassword.innerText = `${password}`;
+    pPassword.innerText = `${localStorage.getItem("elnur@code.edu.az")}`;
     btn.style.display = "none";
   }
-}, 1000);
+  if (localStorage.getItem("ceyhun@gmail.com")) {
+    emailInput.style.display = "none";
+    passwordInput.style.display = "none";
+    pEmail.style.display = "block";
+    pEmail.innerText = `ceyhun@gmail.com`;
+    pPassword.style.display = "block";
+    pPassword.innerText = `${localStorage.getItem("ceyhun@gmail.com")}`;
+    btn.style.display = "none";
+  }
+  if (localStorage.getItem("ali@outlook.com")) {
+    emailInput.style.display = "none";
+    passwordInput.style.display = "none";
+    pEmail.style.display = "block";
+    pEmail.innerText = `ali@outlook.com`;
+    pPassword.style.display = "block";
+    pPassword.innerText = `${localStorage.getItem("ali@outlook.com")}`;
+    btn.style.display = "none";
+  }
+  if (localStorage.getItem("nicat@email.ru")) {
+    emailInput.style.display = "none";
+    passwordInput.style.display = "none";
+    pEmail.style.display = "block";
+    pEmail.innerText = `nicat@email.ru`;
+    pPassword.style.display = "block";
+    pPassword.innerText = `${localStorage.getItem("nicat@email.ru")}`;
+    btn.style.display = "none";
+  }
+  if (localStorage.getItem("aydan@asoiu.edu.az")) {
+    emailInput.style.display = "none";
+    passwordInput.style.display = "none";
+    pEmail.style.display = "block";
+    pEmail.innerText = `aydan@asoiu.edu.az`;
+    pPassword.style.display = "block";
+    pPassword.innerText = `${localStorage.getItem("aydan@asoiu.edu.az")}`;
+    btn.style.display = "none";
+  }
+}, 0);
